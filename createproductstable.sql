8e115669a77cf9b45feeff2041f34e4f3d7a7a01
Anastasiya Kravchenko <staska_1997@mail.ru>
2017-04-12 20:01:19 +0300

    DB  products table

---
+++ createproductstable.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `price` int(11) DEFAULT NULL,
+  `description` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `products_id_uindex` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
