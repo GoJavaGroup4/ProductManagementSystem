52ddd7903d9acbc195b2ed0bb27c481d1c12f8b0
Semen Sizov <sizov.semen@gmail.com>
2017-04-19 17:30:50 +0300

    UserDao changed

--- src/main/java/ua/goit/java/dao/UserDao.java
+++ src/main/java/ua/goit/java/dao/UserDao.java
@@ -1,8 +1,32 @@
 package ua.goit.java.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import ua.goit.java.model.User;
 
-public interface UserDao extends JpaRepository<User, Long> {
-	User findByUsername(String username);
+@Repository
+public class UserDao {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	public User findByUsername(String username) {
+		List<User> users = sessionFactory.getCurrentSession().
+			createQuery("select User from users").list();
+		User userToFind = null;
+		for (User user : users) {
+			if (user.getUsername().equals(username)) {
+				userToFind = user;
+			}
+		}
+		return userToFind;
+	}
+
+	public void save(User user){
+		sessionFactory.getCurrentSession().save(user);
+	}
+
+
 }
