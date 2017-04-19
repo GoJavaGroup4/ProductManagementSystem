f1ad76701cd7e90228e8337115c137f92cd3dd48
Semen Sizov <sizov.semen@gmail.com>
2017-04-19 14:50:15 +0300

    sql for product tables fixed

--- createproductstable.sql
+++ createproductstable.sql
@@ -3,7 +3,7 @@ CREATE TABLE `products` (
   `name` varchar(30) NOT NULL,
   `fabricator` VARCHAR (45) NOT NULL,
   `price` int(11) DEFAULT NULL,
-  `description` varchar(50) DEFAULT NULL,
+  `description` varchar(250) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `products_id_uindex` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
