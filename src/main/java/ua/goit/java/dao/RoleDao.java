3dbb2920ea5d0980b16b1d19bcd65c65a7815207
Semen Sizov <sizov.semen@gmail.com>
2017-04-19 17:20:05 +0300

    RoleDao changed

--- src/main/java/ua/goit/java/dao/RoleDao.java
+++ src/main/java/ua/goit/java/dao/RoleDao.java
@@ -1,8 +1,21 @@
 package ua.goit.java.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import ua.goit.java.model.Role;
 
-public interface RoleDao extends JpaRepository<Role, Long> {
+@Repository
+public class RoleDao{
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	public Role getOne(long l) {
+		Session session = sessionFactory.getCurrentSession();
+		Role role = session.load(Role.class, l);
+		return role;
+	}
 
 }
