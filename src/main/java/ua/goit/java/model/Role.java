92ef080da6e105067e81f6281ddea1b8a60582c1
Semen Sizov <sizov.semen@gmail.com>
2017-04-19 15:10:59 +0300

    added User and Role objects

---
+++ src/main/java/ua/goit/java/model/Role.java
@@ -0,0 +1,70 @@
+package ua.goit.java.model;
+
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+/**
+ * Simple JavaBean object that represents role of {@link User}
+ *
+ * @author Semen
+ * @version 1.0
+ *
+ *
+ */
+
+@Entity
+@Table (name = "roles")
+public class Role {
+
+	@Id
+	@GeneratedValue (strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column (name = "name")
+	private String name;
+
+	@ManyToMany (mappedBy = "roles")
+	private Set<User> users;
+
+	public Role() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(Set<User> users) {
+		this.users = users;
+	}
+
+	@Override
+	public String toString() {
+		return "Role{" +
+			"id=" + id +
+			", name='" + name + '\'' +
+			", users=" + users +
+			'}';
+	}
+}
--- src/main/java/ua/goit/java/model/User.java
+++ src/main/java/ua/goit/java/model/User.java
@@ -1,7 +1,86 @@
 package ua.goit.java.model;
 
-public enum User {
-    ADMIN,
-    USER
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 
+/** Simple JavaBean domain object that represents a User of application
+ *
+ *  @author Semen
+ *  @version 1.0
+ *
+ */
+
+@Entity
+@Table (name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column (name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Transient
+    transient private String confirmPassword;
+
+    @ManyToMany
+    @JoinTable (name = "user_roles", joinColumns = @JoinColumn (name = "user_id"),
+                inverseJoinColumns = @JoinColumn (name = "role_id"))
+    private Set<Role> roles;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
