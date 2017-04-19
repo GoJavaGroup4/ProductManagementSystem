12fe5a7b47bce951017fd0683ddfd1356d69c0ad
Semen Sizov <sizov.semen@gmail.com>
2017-04-19 16:51:26 +0300

    UserController and UserValidator added

---
+++ src/main/java/ua/goit/java/validator/UserValidator.java
@@ -0,0 +1,51 @@
+package ua.goit.java.validator;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+import ua.goit.java.model.User;
+import ua.goit.java.service.UserService;
+
+/**
+ * validator for {@link ua.goit.java.model.User} class,
+ * implements {@link org.springframework.validation.Validator} interface.
+ *
+ * @author Semen
+ * @version 1.0
+ */
+
+@Component
+public class UserValidator implements Validator {
+
+	@Autowired
+	private UserService userService;
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return User.class.equals(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		User user = (User) target;
+
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "userName", "Required");
+
+		if (userService.findUserByName(user.getUsername()) != null) {
+			errors.rejectValue("userName", "Duplicate.userForm.username");
+		}
+
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "Required");
+
+		if (user.getPassword().length() < 8){
+			errors.rejectValue("password", "Size.userForm.password");
+		}
+
+		if (!user.getConfirmPassword().equals(user.getPassword())){
+			errors.rejectValue("confirmPassword", "Different.userForm.password");
+		}
+	}
+}
